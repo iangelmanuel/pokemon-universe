82f673d2539644d90224babdf9be156f4751dc8b
iangelmanuel <iangelmanueldm@gmail.com>
2025-05-24 05:00:06 -0500

    feat: add SEO component and site information for improved metadata handling

---
+++ src/constant/site-info.ts
@@ -0,0 +1,12 @@
+export const siteInfo = {
+  title: "Pokeworld",
+  description:
+    "Pokeworld es un sitio web dedicado a los fanáticos de Pokémon, donde puedes explorar información detallada sobre pokemons, cartas, sets y más.",
+  image: "assets/images/logo.webp",
+  url: "https://pokeworld.com.co",
+  address: "Calle 88 #49c",
+  phone: "+57 317 579 6600",
+  email: "iangelmanuel02@gmail.com",
+  city: "Barranquilla, Atlántico",
+  twitter: "@iangelmanuel"
+}
